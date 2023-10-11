b23bc20424823db221b8f315e8d3b65b7605eaf9
aakash-301 <aakashshivanshu5@gmail.com>
2023-10-11 13:28:22 +0530

    addd navbar and footer

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import Logo from "../img/logo.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="links">
+          <Link to="/?cat=arts" className="link">
+            <h6>ART</h6>
+          </Link>
+          <Link to="/?cat=science" className="link">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link to="/?cat=technology" className="link">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link to="/?cat=cinema" className="link">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link to="/?cat=design" className="link">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link to="/?cat=food" className="link">
+            <h6>FOOD</h6>
+          </Link>
+          <span>Aakash</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
